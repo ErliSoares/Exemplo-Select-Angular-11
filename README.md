90fe744685ed55e7d5304c91fce5e2cba4316c36
ErliSoares <erli-silva@hotmail.com>
2021-02-04 17:12:09 -0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# angular-ivy-jzowju
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/angular-ivy-jzowju)
---
+++ src/app/app.component.ts
@@ -0,0 +1,22 @@
+import { Component } from "@angular/core";
+import { HttpClient } from "@angular/common/http";
+
+@Component({
+  selector: "my-app",
+  templateUrl: "./app.component.html",
+  template: `
+    <select>
+      <option *ngFor="let item of items" [value]="item.title">{{
+        item.title
+      }}</option>
+    </select>
+  `
+})
+export class AppComponent {
+  items: any;
+  constructor(private http: HttpClient) {
+    this.http
+      .get("https://jsonplaceholder.typicode.com/todos")
+      .subscribe(res => (this.items = res));
+  }
+}
